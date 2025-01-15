807f54fbfcd7bd113d6f42d96b1351285e963a4f
Aitor Agirre <AAgirre@ikerlan.es>
2025-01-15 15:07:48 +0100

    Revert "fixes lib.cpp"
    This reverts commit 35b759965ae4f1bc70608b79f2b7f257af5ef00a.

--- src/lib.cpp
+++ src/lib.cpp
@@ -10,12 +10,19 @@ int myfun(bool b, int (*callback)(int)) {
     num = 15 + num;
   }
 
-  if (callback != nullptr) {
+  if (callback != 0) {
+    ;
+    ; // intentional extra-semicolon
+  }
+
+  if (callback == nullptr) {
     return (*callback)(3); // intentional nullptr dereference
   }
   else {
-    return num;
+    return 0;
   }
+
+  //return 42; // intentional dead code
 }
 
 int sumar(int a, int b) {
